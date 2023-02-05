f58247cee306622be5c2c9f4a2bc252cb636d55d
MishaStryzhka <misha.strizhcko@gmail.com>
2023-02-05 21:35:52 +0100

    Update task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+    gallery: document.querySelector('.gallery'),
+};
+const elAll = [];
+// let url;
+let alt;
+
+galleryItems.map(galleryItem => {
+
+    const elA = document.createElement('a');
+    elA.classList.add('gallery__link');
+    elA.setAttribute('href', galleryItem.original);
+    
+    const elImg = document.createElement('img');
+    elImg.classList.add('gallery__image');
+    elImg.setAttribute('src', galleryItem.preview);
+    elImg.setAttribute('data-src', galleryItem.original)
+    elImg.setAttribute('alt', galleryItem.description);
+
+    elA.appendChild(elImg);
+    elAll.push(elA);
+});
+
+refs.gallery.append(...elAll);
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+                captionsData: 'alt',
+                captionDelay: 250,
+            });
+
